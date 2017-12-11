0f37fede8fe7fb948bb1b11895087d5485568b96
Sintari <aleks.bernaciak@gmail.com>
2017-12-11 19:00:38 +0100

    added android and prepared for cordova dev

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,29 +1,9 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-  {
-    "id": "cordova-plugin-network-information.network",
-    "file": "plugins/cordova-plugin-network-information/www/network.js",
-    "pluginId": "cordova-plugin-network-information",
-    "clobbers": [
-      "navigator.connection",
-      "navigator.network.connection"
-    ]
-  },
-  {
-    "id": "cordova-plugin-network-information.Connection",
-    "file": "plugins/cordova-plugin-network-information/www/Connection.js",
-    "pluginId": "cordova-plugin-network-information",
-    "clobbers": [
-      "Connection"
-    ]
-  }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-browsersync": "0.1.7",
-  "cordova-plugin-whitelist": "1.3.2",
-  "cordova-plugin-network-information": "1.3.4"
+  "cordova-plugin-browsersync": "0.1.7"
 };
 // BOTTOM OF METADATA
 });
